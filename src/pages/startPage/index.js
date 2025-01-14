0eb25d5c45c1ee9979f5f1f639b1295ae4c269bd
Natanna99 <erzafairy79@gmail.com>
2025-01-14 16:27:15 -0300

    feature: criacao da pagina inicial

--- src/pages/startPage/index.js
+++ src/pages/startPage/index.js
@@ -1,3 +1,17 @@
+import React from "react";
+import style from "./startPage.module.css";
+import posts from "../../json/posts.json";
+
+import { Card } from "../../components/card";
+
 export default function StartPage() {
-  return <div>Pagina de inicio</div>;
+  return (
+    <div>
+      <div className={style.containerBody}>
+        {posts?.map((card) => (
+          <Card key={card.id} info={card} />
+        ))}
+      </div>
+    </div>
+  );
 }
