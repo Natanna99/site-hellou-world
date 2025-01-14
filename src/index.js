383de1dcdaa32da20d80962121f896eac55d47ea
Natanna99 <erzafairy79@gmail.com>
2025-01-14 16:32:25 -0300

    Feature: aplicao da lib de fontawesome no projeto

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import AppRoutes from "./routes";
+import "./font";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
