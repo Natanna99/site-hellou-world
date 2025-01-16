b831710fb71d0c69ec19b7f45c7c9954d1d13371
Natanna99 <erzafairy79@gmail.com>
2025-01-16 11:46:56 -0300

    Chore: add children

--- src/components/bodyPage/index.js
+++ src/components/bodyPage/index.js
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { Banner } from "../banner";
 
-export default function BodyPage() {
+export default function BodyPage({ children }) {
   return (
     <div>
       <Banner />
       <Outlet />
+      {children}
     </div>
   );
 }
