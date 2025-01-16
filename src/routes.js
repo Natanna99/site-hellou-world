88da1037ccc1d17016731650cb0c54c536ed3e39
Natanna99 <erzafairy79@gmail.com>
2025-01-16 11:47:52 -0300

    Chore: ajuste e adicao de rotas

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Menu from "./components/menu";
 import { Footer } from "./components/footer";
 import BodyPage from "./components/bodyPage";
+import Post from "./pages/post";
+import Error404 from "./pages/error404";
 
 function AppRoutes() {
   return (
@@ -12,9 +14,11 @@ function AppRoutes() {
       <Menu />
       <Routes>
         <Route path="/" element={<BodyPage />}>
-          <Route path="/" element={<StartPage />} />
-          <Route path="/aboutme" element={<AboutMe />} />
+          <Route index element={<StartPage />} />
+          <Route path="aboutme" element={<AboutMe />} />
         </Route>
+        <Route path="*" element={<Error404 />} />
+        <Route path="posts/:id/*" element={<Post />} />
       </Routes>
       <Footer />
     </BrowserRouter>
